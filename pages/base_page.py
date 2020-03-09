eaa21f2cbab7049879191338070d4f5f859e750d
kharinandrii <39236584+kharinandrii@users.noreply.github.com>
2020-03-09 23:25:17 +0200

    "add readme" "initial commit" "simple test case added" "homework from previous module" added method is_element_present in base_page - this method checked element on page

---
+++ pages/base_page.py
@@ -0,0 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
+
+class BasePage():
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self,type_locator, element):
+        try:
+            self.browser.find_element(type_locator, element)
+        except(NoSuchElementException):
+            return False
+        return True
